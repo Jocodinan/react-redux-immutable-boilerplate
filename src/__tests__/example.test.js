4297425829311a0d9100b60bbcbef97905367424
jocodinan <jorge.codina@gmail.com>
2018-10-08 16:45:51 -0300

    testing test react component

--- src/__tests__/example.test.js
+++ src/__tests__/example.test.js
@@ -1,3 +1,11 @@
-it('sums numbers', ()=>{
-  expect(1 + 1).toEqual(2)
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Test from '../components/test/test.jsx';
+
+configure({ adapter: new Adapter() });
+
+it('renders Test component', ()=>{
+  const wrapper = shallow(<Test />);
+  expect(wrapper.contains(<h1>Test</h1>)).toEqual(true);
 });
